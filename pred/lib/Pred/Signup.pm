4467fc4a47cd85c6e10f9747f52d02b0ab5bedce
Kazuhiro Shibuya <stevenlabs@gmail.com>
2010-01-25 03:06:21 +0900

    Adding encrypt password process.

--- pred/lib/Pred/Signup.pm
+++ pred/lib/Pred/Signup.pm
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 
 use base 'Mojolicious::Controller';
+use String::Random qw(random_string);
+use Digest::SHA::PurePerl;
 
 sub new {
     my $self = shift;
@@ -12,7 +14,13 @@ sub new {
 
 sub register {
     my $self = shift;
-    $self->render;
+
+    # salt is 8 character
+    my $salt = random_string("........");
+    my $pass = $self->req->param("password");
+    my $hashed = sha512_hex($salt . $pass);
+
+    $self->render();
 }
 
 1;
