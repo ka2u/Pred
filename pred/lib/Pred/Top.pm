642f8e593ac048c35af78edfd4d64a51fc2102f1
Kazuhiro Shibuya <stevenlabs@gmail.com>
2010-01-27 02:30:29 +0900

    Added login.

--- pred/lib/Pred/Top.pm
+++ pred/lib/Pred/Top.pm
@@ -12,7 +12,26 @@ sub index {
 
 sub login {
     my $self = shift;
-    $self->render;
+
+    my $itr = $self->app->db->get(
+        users => { 
+            where => [name => $self->req->param('id')] 
+        },
+    );
+
+    if ($itr->has_next) {
+        my $user = $itr->next;
+        my $hashed = $self->app->util->hash(
+            $user->salt, 
+            $self->req->param('password'));
+
+        if ($user->password eq $hashed) {
+            $self->redirect_to('/dashboard/index');
+            return;
+        }
+    }
+
+    $self->redirect_to('/top/index');
 }
 
 sub logout {
